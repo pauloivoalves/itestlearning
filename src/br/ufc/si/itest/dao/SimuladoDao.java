ffc1638052d5dc2e81e278ad4e54dba3da4eaf8d
mardsonferreira25 <mardsonferreira25@gmail.com>
2013-07-09 16:56:57 +0000

    as classes relacionados ao simulado.

---
+++ src/br/ufc/si/itest/dao/SimuladoDao.java
@@ -0,0 +1,27 @@
+package br.ufc.si.itest.dao;
+
+import java.util.Date;
+import java.util.List;
+
+import br.ufc.si.itest.model.Professor;
+import br.ufc.si.itest.model.Simulado;
+
+public interface SimuladoDao {
+
+	
+	 public void save(Simulado simulado);
+	 
+	 public void update(Simulado simulado);
+	 
+	 public void delete(Simulado simulado);
+	 
+	 public Simulado getSimuladoById(int id);
+	 
+	 public Simulado getSimuladoByData(Date data);
+	 
+	 public List<Simulado> list(int prof_id);
+	 
+	 public List<Simulado> listSimulados(Professor professor);
+	 
+	 
+}
