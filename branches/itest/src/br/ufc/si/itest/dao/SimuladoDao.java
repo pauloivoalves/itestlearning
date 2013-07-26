d06fa51dddbf265bce8e829ca0571718f8e6ef83
felipe.freitas.jorge@gmail.com <felipe.freitas.jorge@gmail.com@c8c57014-e02d-97c7-8d23-81c01d508a00>
2013-07-26 12:31:40 +0000

    Adicionado metodos para logica de simulado

--- branches/itest/src/br/ufc/si/itest/dao/SimuladoDao.java
+++ branches/itest/src/br/ufc/si/itest/dao/SimuladoDao.java
@@ -8,20 +8,19 @@ import br.ufc.si.itest.model.Simulado;
 
 public interface SimuladoDao {
 
-	
-	 public void save(Simulado simulado);
-	 
-	 public void update(Simulado simulado);
-	 
-	 public void delete(Simulado simulado);
-	 
-	 public Simulado getSimuladoById(int id);
-	 
-	 public Simulado getSimuladoByData(Date data);
-	 
-	 public List<Simulado> list(int prof_id);
-	 
-	 public List<Simulado> listSimulados(Professor professor);
-	 
-	 
+	public void save(Simulado simulado);
+
+	public void update(Simulado simulado);
+
+	public void delete(Simulado simulado);
+
+	public Simulado getSimuladoById(int id);
+
+	public Simulado getSimuladoByData(Date data);
+
+	public List<Simulado> list(int prof_id);
+
+	public List<Simulado> listSimulados(Professor professor);
+
+	public List<Simulado> getSimuladoByTurma(int turma_id);
 }
