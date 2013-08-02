0dd8b17a4945ce2b20a4403198a1ab3dc9ed7b79
mardsonferreira25@gmail.com <mardsonferreira25@gmail.com@c8c57014-e02d-97c7-8d23-81c01d508a00>
2013-08-02 13:15:53 +0000

    gerando pdf dos jogos alunos.

--- branches/itest/src/br/ufc/si/itest/bean/ProfessorBean.java
+++ branches/itest/src/br/ufc/si/itest/bean/ProfessorBean.java
@@ -1,17 +1,19 @@
 package br.ufc.si.itest.bean;
 
+import java.awt.Color;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
 import javax.faces.model.SelectItem;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import javax.swing.ImageIcon;
 
 import br.ufc.si.itest.dao.AlunoDao;
 import br.ufc.si.itest.dao.AlunoTurmaDao;
@@ -38,7 +40,7 @@ import br.ufc.si.itest.model.Turma;
 import br.ufc.si.itest.utils.SendMail;
 
 import com.lowagie.text.Document;
-import com.lowagie.text.DocumentException;
+import com.lowagie.text.Element;
 import com.lowagie.text.Font;
 import com.lowagie.text.Image;
 import com.lowagie.text.Phrase;
@@ -63,7 +65,6 @@ public class ProfessorBean {
 	private List<Simulado> simulados;
 	private List<Projeto> projetos;
 	private Projeto projeto;
-	
 
 	public boolean isContains_alunos() {
 		if (alunos.isEmpty())
@@ -170,7 +171,6 @@ public class ProfessorBean {
 	public void setSimulados(List<Simulado> simulados) {
 		this.simulados = simulados;
 	}
-	
 
 	public List<Projeto> getProjetos() {
 		return projetos;
@@ -210,14 +210,12 @@ public class ProfessorBean {
 				false);
 
 		professor = (Professor) session.getAttribute("prof");
-		
+
 		try {
 			carregarTurmas();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-
-		
 
 	}
 
@@ -293,19 +291,19 @@ public class ProfessorBean {
 		return itensSimulado;
 	}
 
-	 public List<SelectItem> getProjetosSelecao(){
-		 List<SelectItem> itensProjeto = new ArrayList<SelectItem>();
-		 
-		 ProjetoDao projDao = new ProjetoDaoImpl();
-		 projetos = projDao.list();
-		 
-		 for(Projeto p:projetos){
-			 itensProjeto.add(new SelectItem(p.getId(),p.getNome()));
-		 }
-		 
-		 return itensProjeto;
-		 
-	 }
+	public List<SelectItem> getProjetosSelecao() {
+		List<SelectItem> itensProjeto = new ArrayList<SelectItem>();
+
+		ProjetoDao projDao = new ProjetoDaoImpl();
+		projetos = projDao.list();
+
+		for (Projeto p : projetos) {
+			itensProjeto.add(new SelectItem(p.getId(), p.getNome()));
+		}
+
+		return itensProjeto;
+
+	}
 
 	public String AdicionarTurma() {
 		TurmaDao turmaDao = new TurmaDaoImpl();
@@ -467,104 +465,332 @@ public class ProfessorBean {
 		jogos = jogo_dao.getJogoByUsuario(aluno.getId());
 		return "prof_visual_jogos_aluno.jsf";
 	}
-	
-	
-	public String CadastrarSimulado(){
+
+	public String CadastrarSimulado() {
 		SimuladoDao simDao = new SimuladoDaoImpl();
 		ProjetoDao projDao = new ProjetoDaoImpl();
 		TurmaDao turmaDao = new TurmaDaoImpl();
-		
+
 		projeto = projDao.getProjetoById(projeto.getId());
 		turma = turmaDao.getTurmaById(turma.getId());
-		
+
 		simulado.setProjeto(projeto);
 		simulado.setTurma(turma);
 		simulado.setProfessor(professor);
-	
-		
+
 		simDao.save(simulado);
-		
+
 		simulados = simDao.list(professor.getId());
-		
+
 		return "prof_visual_simulados.jsf";
 	}
 
-	public String visualizarSimulados(){
+	public String visualizarSimulados() {
 		SimuladoDao simDao = new SimuladoDaoImpl();
-		
+
 		simulados = simDao.list(professor.getId());
-		
-		System.out.println(simulados == null);
+
 		return "prof_visual_simulados.jsf";
 	}
-	
-	
-	public String gerarRelatórioTurma(){
-		
+
+	public String resultadoSimulado() {
+		alunos.clear();
+		TurmaDao tdao = new TurmaDaoImpl();
+		turma = tdao.getTurmaById(simulado.getTurma().getId());
+
+		AlunoTurmaDao atd = new AlunoTurmaDaoImpl();
+		List<AlunoTurma> at = atd.getAlunoTurmaByIdTurma(this.turma.getId());
+
+		for (AlunoTurma elem : at) {
+			alunos.add(elem.getPk().getAluno());
+		}
+
+		return "prof_visual_res_simulado.jsf";
+	}
+
+	public void rankingGeral(ActionEvent event) throws IOException {
+
+		jogos = new ArrayList<Jogo>();
+		JogoDao jogoDao = new JogoDaoImpl();
+		ProjetoDao projetoDao = new ProjetoDaoImpl();
+		jogos = jogoDao.getJogoByProjeto(projetoDao.getProjetoByName(
+				simulado.getProjeto().getNome()).getId());
+		FacesContext.getCurrentInstance().getExternalContext()
+				.redirect("prof_visual_jogos_aluno.jsf");
+
+	}
+
+	public void rankingTurma(ActionEvent event) throws IOException {
+
+		jogos = new ArrayList<Jogo>();
+		JogoDao jogoDao = new JogoDaoImpl();
+		ProjetoDao projetoDao = new ProjetoDaoImpl();
+		jogos = jogoDao.getJogoByTurmaProjeto(simulado.getTurma().getId(),
+				projetoDao.getProjetoByName(simulado.getProjeto().getNome())
+						.getId());
+		FacesContext.getCurrentInstance().getExternalContext()
+				.redirect("prof_visual_jogos_aluno.jsf");
+
+	}
+
+	public String gerarRelatórioTurma() {
+
 		TurmaDao tdao = new TurmaDaoImpl();
 		this.turma = tdao.getTurmaById(turma.getId());
 		AlunoDao adao = new AlunoDaoImpl();
-		
-		this.alunos = adao.listAlunosByTurma(turma.getId());
-		
-		try{
-			//pega o contexto da aplicação, caso queria salvar o arquivo em um diretório
-		ServletContext ctx = (ServletContext) FacesContext.getCurrentInstance().getExternalContext().getContext();
-		
-		//pega o contexto da resposta da requisição
-		FacesContext fc = FacesContext.getCurrentInstance();
-		HttpServletResponse response = (HttpServletResponse) fc.getCurrentInstance().getExternalContext().getResponse();
-		response.setContentType("application/pdf");
-		
-		Document document = new Document();
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        PdfWriter.getInstance(document, baos);
-        document.open();
-        Font fonteCabecalho = new Font(Font.NORMAL, 12, Font.BOLD); /* Será usada no cabeçalho. */
-        Font fonteDesc = new Font(Font.NORMAL, 11, Font.BOLD); /* Será usada na descrição. */
-        Font fonteConteudo = new Font(Font.NORMAL, 12, Font.NORMAL); /* Será usada no corpo de relatório. */
 
-        /* Tabela para o cabeçalho. */
-        PdfPTable cabecalho = new PdfPTable(2);
-        float[] widths = {0.15f, 0.85f};
-        cabecalho.setWidthPercentage(90);
-        cabecalho.setWidths(widths);
-        cabecalho.getDefaultCell().setBorder(PdfPCell.NO_BORDER);
-        cabecalho.addCell(new Phrase("Alunos Participantes da Turma" + this.turma, fonteCabecalho));
-        document.add(cabecalho);
+		System.out.println(turma.getNome());
 
-        PdfPTable es = new PdfPTable(1);
-        float[] width = {0.85f};
-        es.setWidthPercentage(100);
-        es.setWidths(width);
-        es.getDefaultCell().setBorder(PdfPCell.NO_BORDER);
-        es.addCell(new Phrase(turma.getNome(), fonteDesc));
-        document.add(es);
-        
-        
-        document.close();
-        response.setContentLength(baos.size());
-        ServletOutputStream out = null;
-        try {
-            out = response.getOutputStream();
-        } catch (IOException ex) {
-            ex.printStackTrace();
-            return "prof_visual_simulados.jsf";
-        }
-        try {
-            baos.writeTo(out);
-            out.flush();
-            out.close();
-        } catch (Exception ex) {
-            ex.printStackTrace();
-            return "prof_visual_simulados.jsf";
-        }
-    } catch (Exception ex) {
-        ex.printStackTrace();
-        return "prof_visual_simulados.jsf";
-    }
+		AlunoTurmaDao aluno_turma_dao = new AlunoTurmaDaoImpl();
+
+		List<AlunoTurma> alunosTurma = new ArrayList<AlunoTurma>();
+
+		alunosTurma = aluno_turma_dao.getAlunoTurmaByIdTurma(turma.getId());
+
+		alunos.clear();
+
+		for (AlunoTurma at : alunosTurma) {
+			alunos.add(at.getPk().getAluno());
+		}
+
+		System.out.println(alunos.size());
+
+		try {
+			// pega o contexto da aplicação, caso queria salvar o arquivo em um
+			// diretório
+			ServletContext ctx = (ServletContext) FacesContext
+					.getCurrentInstance().getExternalContext().getContext();
+
+			// pega o contexto da resposta da requisição
+			FacesContext fc = FacesContext.getCurrentInstance();
+			HttpServletResponse response = (HttpServletResponse) fc
+					.getCurrentInstance().getExternalContext().getResponse();
+			HttpServletRequest request = (HttpServletRequest) fc
+					.getCurrentInstance().getExternalContext().getRequest();
+
+			response.setContentType("application/pdf");
+
+			Document document = new Document();
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			PdfWriter.getInstance(document, baos);
+			document.open();
+
+			Image jpg = Image.getInstance("http://" + request.getServerName()
+					+ ":" + request.getServerPort() + request.getContextPath()
+					+ "/images/UFC.png");
+			jpg.setAlignment(Image.LEFT | Image.UNDERLYING); /*
+															 * Ajusta o
+															 * alinhamento da
+															 * imagem.
+															 */
+
+			Font fonteCabecalho = new Font(Font.TIMES_ROMAN, 12, Font.BOLD); /*
+																			 * Será
+																			 * usada
+																			 * no
+																			 * cabeçalho
+																			 * .
+																			 */
+			Font fonteDesc = new Font(Font.NORMAL, 11, Font.BOLD); /*
+																	 * Será
+																	 * usada na
+																	 * descrição
+																	 * .
+																	 */
+			Font fonteConteudo = new Font(Font.NORMAL, 12, Font.NORMAL); /*
+																		 * Será
+																		 * usada
+																		 * no
+																		 * corpo
+																		 * de
+																		 * relatório
+																		 * .
+																		 */
+
+			/* Tabela para o cabeçalho. */
+			PdfPTable cabecalho = new PdfPTable(2);
+			float[] widths = { 0.15f, 0.85f };
+			cabecalho.setWidthPercentage(90);
+			cabecalho.setWidths(widths);
+			cabecalho.getDefaultCell().setBorder(PdfPCell.NO_BORDER);
+
+			cabecalho.addCell(jpg);
+			cabecalho.addCell(new Phrase("Alunos Participantes da Turma de "
+					+ this.turma.getNome(), fonteCabecalho));
+
+			document.add(cabecalho);
+
+			PdfPTable table = new PdfPTable(3);
+			table.setSpacingBefore(5f); /* Coloca um espaço antes da tabela. */
+			table.setWidthPercentage(90);
+			table.getDefaultCell().setBorder(PdfPCell.NO_BORDER);
+			widths = new float[] { 0.05f, 0.50f, 0.20f };
+			table.setWidths(widths);
+			table.getDefaultCell().setGrayFill(0.5f);
+
+			table.addCell(new Phrase("Nº", fonteDesc));
+			table.addCell(new Phrase("Nome", fonteDesc));
+			table.addCell(new Phrase("E-mail", fonteDesc));
+
+			for (int i = 0; alunos != null && i < alunos.size(); i++) {
+				if (i % 2 == 0) {
+					table.getDefaultCell().setBackgroundColor(Color.WHITE);
+				} else {
+					table.getDefaultCell().setGrayFill(0.80f);
+				}
+				Aluno a = alunos.get(i);
+				table.addCell(new Phrase("" + (i + 1), fonteConteudo));
+				table.addCell(new Phrase(a.getNome(), fonteConteudo));
+				table.addCell(new Phrase(a.getLogin(), fonteConteudo));
+			}
+			document.add(table);
+
+			document.close();
+			response.setContentLength(baos.size());
+			ServletOutputStream out = null;
+			try {
+				out = response.getOutputStream();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+				return "prof_visual_simulados.jsf";
+			}
+			try {
+				baos.writeTo(out);
+				out.flush();
+				out.close();
+			} catch (Exception ex) {
+				ex.printStackTrace();
+				return "prof_visual_simulados.jsf";
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			return "prof_visual_simulados.jsf";
+		}
 		return "prof_visual_simulados.jsf";
-		
+
 	}
-	
+
+	public String RelatórioJogosAluno() {
+
+		try {
+			// pega o contexto da aplicação, caso queria salvar o arquivo em um
+			// diretório
+			ServletContext ctx = (ServletContext) FacesContext
+					.getCurrentInstance().getExternalContext().getContext();
+
+			// pega o contexto da resposta da requisição
+			FacesContext fc = FacesContext.getCurrentInstance();
+			HttpServletResponse response = (HttpServletResponse) fc
+					.getCurrentInstance().getExternalContext().getResponse();
+			HttpServletRequest request = (HttpServletRequest) fc
+					.getCurrentInstance().getExternalContext().getRequest();
+
+			response.setContentType("application/pdf");
+
+			Document document = new Document();
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			PdfWriter.getInstance(document, baos);
+			document.open();
+
+			Image jpg = Image.getInstance("http://" + request.getServerName()
+					+ ":" + request.getServerPort() + request.getContextPath()
+					+ "/images/UFC.png");
+			jpg.setAlignment(Image.LEFT | Image.UNDERLYING); /*
+															 * Ajusta o
+															 * alinhamento da
+															 * imagem.
+															 */
+
+			Font fonteCabecalho = new Font(Font.TIMES_ROMAN, 12, Font.BOLD); /*
+																			 * Será
+																			 * usada
+																			 * no
+																			 * cabeçalho
+																			 * .
+																			 */
+			Font fonteDesc = new Font(Font.NORMAL, 11, Font.BOLD); /*
+																	 * Será
+																	 * usada na
+																	 * descrição
+																	 * .
+																	 */
+			Font fonteConteudo = new Font(Font.NORMAL, 12, Font.NORMAL); /*
+																		 * Será
+																		 * usada
+																		 * no
+																		 * corpo
+																		 * de
+																		 * relatório
+																		 * .
+																		 */
+
+			/* Tabela para o cabeçalho. */
+			PdfPTable cabecalho = new PdfPTable(2);
+			float[] widths = { 0.15f, 0.85f };
+			cabecalho.setWidthPercentage(90);
+			cabecalho.setWidths(widths);
+			cabecalho.getDefaultCell().setBorder(PdfPCell.NO_BORDER);
+
+			cabecalho.addCell(jpg);
+			cabecalho.addCell(new Phrase("Relatório dos Projetos do Aluno "
+					+ aluno.getNome(), fonteCabecalho));
+
+			document.add(cabecalho);
+
+			PdfPTable table = new PdfPTable(6);
+			table.setSpacingBefore(5f); /* Coloca um espaço antes da tabela. */
+			table.setWidthPercentage(90);
+			table.getDefaultCell().setBorder(PdfPCell.NO_BORDER);
+			widths = new float[] { 0.05f, 0.50f, 0.20f, 0.50f, 0.20f, 0,50f };
+			table.setWidths(widths);
+			table.getDefaultCell().setGrayFill(0.5f);
+
+			table.addCell(new Phrase("Nº", fonteDesc));
+			table.addCell(new Phrase("Nome", fonteDesc));
+			table.addCell(new Phrase("Projeto", fonteDesc));
+			table.addCell(new Phrase("Nível Dificuldade", fonteDesc));
+			table.addCell(new Phrase("Pontuação", fonteDesc));
+			table.addCell(new Phrase("Data", fonteDesc));
+
+			for (int i = 0; jogos != null && i < jogos.size(); i++) {
+				if (i % 2 == 0) {
+					table.getDefaultCell().setBackgroundColor(Color.WHITE);
+				} else {
+					table.getDefaultCell().setGrayFill(0.80f);
+				}
+				Jogo j = jogos.get(i);
+				table.addCell(new Phrase("" + (i + 1), fonteConteudo));
+				table.addCell(new Phrase(j.getPk().getUsuario().getNome(), fonteConteudo));
+				table.addCell(new Phrase(j.getPk().getProjeto().getNome(), fonteConteudo));
+				table.addCell(new Phrase(j.getPk().getProjeto().getNivelDificuldade().getNome(), fonteConteudo));
+				table.addCell(new Phrase( "" + j.getPontuacao(), fonteConteudo));
+				table.addCell(new Phrase("" + j.getData(), fonteConteudo));
+			}
+			document.add(table);
+
+			document.close();
+			response.setContentLength(baos.size());
+			ServletOutputStream out = null;
+			try {
+				out = response.getOutputStream();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+				return "prof_visual_jogos_aluno.jsf";
+			}
+			try {
+				baos.writeTo(out);
+				out.flush();
+				out.close();
+			} catch (Exception ex) {
+				ex.printStackTrace();
+				return "prof_visual_jogos_aluno.jsf";
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			return "prof_visual_jogos_aluno.jsf";
+		}
+		return "prof_visual_jogos_aluno.jsf";
+
+	}
+
 }
