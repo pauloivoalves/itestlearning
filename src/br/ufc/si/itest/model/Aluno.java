5735cb0ced6c87260d7850322570ea3275945c97
felipe.freitas.jorge <felipe.freitas.jorge@gmail.com>
2013-08-29 13:53:50 +0000

    Consertando alguns problemas.

--- src/br/ufc/si/itest/model/Aluno.java
+++ src/br/ufc/si/itest/model/Aluno.java
@@ -3,13 +3,11 @@ package br.ufc.si.itest.model;
 import java.io.Serializable;
 import java.util.List;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
